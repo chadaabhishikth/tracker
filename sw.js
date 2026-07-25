69d4b0fc006adaf3db39822de7fdd0ca2a58956d
Chada S V N S K  Abhishikth <chadaabhishikth@gmail.com>
2026-07-25 07:48:35 +0530

    For notification and previous logging

--- sw.js
+++ sw.js
@@ -61,3 +61,22 @@ self.addEventListener('fetch', (e) => {
   }
 });
 
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+            break;
+          }
+        }
+        return client.focus();
+      }
+      return self.clients.openWindow('./index.html');
+    })
+  );
+});
+
